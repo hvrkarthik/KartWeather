0d655fad5457ee29f67191677af1b3f820049895
hvrkarthik <hvrkarthik@gmail.com>
2023-05-31 20:44:39 +0530

    Recent Changes

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 import Weather from './src/Weather';
