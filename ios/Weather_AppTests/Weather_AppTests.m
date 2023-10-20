442364a8671bc32bf3842dc59dcbeae124fe0357
karthiknippon <138197479+karthiknippon@users.noreply.github.com>
2023-10-20 17:05:55 +0530

    Updated App Name

--- ios/Weather_AppTests/Weather_AppTests.m
+++ ios/Weather_AppTests/Weather_AppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface Weather_AppTests : XCTestCase
+@interface SkySenseTests : XCTestCase
 
 @end
 
-@implementation Weather_AppTests
+@implementation SkySenseTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
